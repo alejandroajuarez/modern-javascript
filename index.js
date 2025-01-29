eec68d73f251db71b53b0b845a6c0e5080b64d70
alejandrojuarez117 <aljuarez117@gmail.com>
2025-01-28 20:20:51 -0700

    add bable

--- index.js
+++ index.js
@@ -10,3 +10,14 @@ console.log(sqrt(-4).toString())
 
 var name = "Bob", time = "today";
 console.log(`Hello ${name}, how are you ${time}?`);
+
+
+// var bob = {
+//   _name: "Bob",
+//   _friends: [],
+//   printFriends() {
+//     this._friends.forEach(f =>
+//       console.log(this._name + " knows " + f));
+//   }
+// };
+// console.log(bob);
