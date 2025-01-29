eb22efb99395c7135685494f0faefd1b43363c9a
alejandrojuarez117 <aljuarez117@gmail.com>
2025-01-28 19:08:22 -0700

    add babel, webpack, and mathjs

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
+var moment = require('moment');
+const { sqrt } = require('mathjs');
+
 console.log("Hello from JS");
 
 var currentDateTime = moment().format('LLLL');
 console.log(currentDateTime);
+
+console.log(sqrt(-4).toString())
+
+var name = "Bob", time = "today";
+console.log(`Hello ${name}, how are you ${time}?`);
